d93857f3f44c86a167e56fb55fba3870249e76b8
Nikolay Ivanov <eoneof@yandex.ru>
2022-09-21 16:39:45 +0300

    fix: add bearer to request header

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -4,16 +4,17 @@ export const apiConfig = {
   LIKES: 'likes',
   AVATAR: 'avatar',
   REQ_HEADERS: {
-    authorization: localStorage.getItem('jwt'),
+    authorization: `Bearer ${localStorage.getItem('jwt')}`,
     'content-type': 'application/json',
   },
+
   API_URL: 'http://127.0.0.1:3001',
   LOGIN: 'signin',
   REGISTER: 'signup',
   USER: 'users/me',
   RES_HEADERS: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
+    accept: 'application/json',
+    'content-type': 'application/json',
   },
 };
 
