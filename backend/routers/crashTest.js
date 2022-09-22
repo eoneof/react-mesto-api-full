03ca6d1e4302410906ea79289bccc90be07675c9
Nikolay Ivanov <eoneof@yandex.ru>
2022-09-23 01:54:20 +0300

    style: add indent

--- backend/routers/crashTest.js
+++ backend/routers/crashTest.js
@@ -3,7 +3,7 @@
 const crashTest = require('express').Router();
 
 module.exports = crashTest
- .get('/crash-test', () => {
+  .get('/crash-test', () => {
     setTimeout(() => {
       throw new Error('Сервер сейчас упадёт');
     }, 0);
