0c5489a3b9559f44c353c863bfac4ab7f47aee46
Nikolay Ivanov <eoneof@yandex.ru>
2022-09-25 17:17:17 +0300

    feat: check public request methods

--- backend/src/routers/auth.js
+++ backend/src/routers/auth.js
@@ -2,7 +2,10 @@ const publicRouter = require('express').Router();
 
 const { validateUserCredentials } = require('../middlewares/validators');
 const { login, createUser } = require('../controllers/authorization');
+const { checkPublicRequests } = require('../utils/utils');
 
 module.exports = publicRouter
+  .get('/signin', checkPublicRequests)
+  .get('/signup', checkPublicRequests)
   .post('/signin', validateUserCredentials, login)
   .post('/signup', validateUserCredentials, createUser);
