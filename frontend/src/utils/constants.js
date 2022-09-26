c2b7d6e6bff4475b7283cc0b6f62c33b51733f83
Nikolay Ivanov <eoneof@yandex.ru>
2022-09-26 13:01:04 +0300

    fix: remove protocol

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -13,7 +13,7 @@ export const apiConfig = {
     'content-type': 'application/json',
   },
 
-  API_URL: isDev() ? 'http://127.0.0.1:3001' : 'https://api.eoneof.nomoredomains.sbs',
+  API_URL: isDev() ? '127.0.0.1:3001' : 'api.eoneof.nomoredomains.sbs',
   LOGIN: 'signin',
   REGISTER: 'signup',
   USER: 'users/me',
